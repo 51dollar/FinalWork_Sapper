92f2ca5501d75de7ddbf29bfdeb92d7825991c88
IgorVolovsky <114361154+IgorVolovsky@users.noreply.github.com>
2022-10-05 16:50:10 +0300

    Add files via upload

---
+++ Miner/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Miner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.GameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ResetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HandSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AProggramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HotKeysToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.RulesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.GameToolStripMenuItem,
+            this.SettingsToolStripMenuItem,
+            this.AboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // GameToolStripMenuItem
+            // 
+            this.GameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewGameToolStripMenuItem,
+            this.ResetToolStripMenuItem});
+            this.GameToolStripMenuItem.Name = "GameToolStripMenuItem";
+            this.GameToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.GameToolStripMenuItem.Text = "Игра";
+            // 
+            // NewGameToolStripMenuItem
+            // 
+            this.NewGameToolStripMenuItem.Name = "NewGameToolStripMenuItem";
+            this.NewGameToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.NewGameToolStripMenuItem.Size = new System.Drawing.Size(225, 26);
+            this.NewGameToolStripMenuItem.Text = "Новая игра";
+            // 
+            // ResetToolStripMenuItem
+            // 
+            this.ResetToolStripMenuItem.Name = "ResetToolStripMenuItem";
+            this.ResetToolStripMenuItem.Size = new System.Drawing.Size(225, 26);
+            this.ResetToolStripMenuItem.Text = "Начать заново";
+            this.ResetToolStripMenuItem.Click += new System.EventHandler(this.ResetToolStripMenuItem_Click);
+            // 
+            // SettingsToolStripMenuItem
+            // 
+            this.SettingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.HardToolStripMenuItem,
+            this.HandSettingsToolStripMenuItem});
+            this.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem";
+            this.SettingsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.SettingsToolStripMenuItem.Size = new System.Drawing.Size(98, 24);
+            this.SettingsToolStripMenuItem.Text = "Настройки";
+            // 
+            // HardToolStripMenuItem
+            // 
+            this.HardToolStripMenuItem.Name = "HardToolStripMenuItem";
+            this.HardToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.HardToolStripMenuItem.Text = "Сложность";
+            // 
+            // HandSettingsToolStripMenuItem
+            // 
+            this.HandSettingsToolStripMenuItem.Name = "HandSettingsToolStripMenuItem";
+            this.HandSettingsToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.HandSettingsToolStripMenuItem.Text = "Ручные настройки";
+            // 
+            // AboutToolStripMenuItem
+            // 
+            this.AboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AProggramToolStripMenuItem,
+            this.HotKeysToolStripMenuItem,
+            this.RulesToolStripMenuItem});
+            this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
+            this.AboutToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.I)));
+            this.AboutToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.AboutToolStripMenuItem.Text = "Справка";
+            // 
+            // AProggramToolStripMenuItem
+            // 
+            this.AProggramToolStripMenuItem.Name = "AProggramToolStripMenuItem";
+            this.AProggramToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.AProggramToolStripMenuItem.Text = "О программе";
+            this.AProggramToolStripMenuItem.Click += new System.EventHandler(this.AProggramToolStripMenuItem_Click);
+            // 
+            // HotKeysToolStripMenuItem
+            // 
+            this.HotKeysToolStripMenuItem.Name = "HotKeysToolStripMenuItem";
+            this.HotKeysToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.HotKeysToolStripMenuItem.Text = "Горячие клавиши";
+            // 
+            // RulesToolStripMenuItem
+            // 
+            this.RulesToolStripMenuItem.Name = "RulesToolStripMenuItem";
+            this.RulesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.RulesToolStripMenuItem.Text = "Правила";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(50, 50);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.menuStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem GameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem NewGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ResetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HandSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AProggramToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HotKeysToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem RulesToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Miner/Form1.cs
@@ -0,0 +1,53 @@
+﻿using Miner.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Miner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ConfigureMapSize();
+            LoadImages();
+            MapController.GetImage = getImage;
+            MapController.Init(this);
+        }
+        private void ConfigureMapSize()
+        {
+            Width = MapController.mapSize * MapController.cellSize + 20;
+            Height = (MapController.mapSize + 1) * MapController.cellSize + 15;
+        }
+        private void AProggramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Игра сапер. Разработана командой #4: Игорь, Дмитрий, Иван");
+        }
+        private void LoadImages()
+        {
+            for (int i = 1; i <= 13; i ++)
+            {
+                imageList1.Images.Add(Image.FromFile(new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.FullName.ToString() + $"\\Sprites\\{i}.png"));
+            }
+        }
+        public Image getImage(int i)
+        {
+            return imageList1.Images[i];
+        }
+
+        private void ResetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MapController.Restart();
+        }
+    }
+}
